dd515863d698d086a70f536564b10dc777c7fe8f
Benfang Wang <bwang54@jhu.edu>
2019-02-05 17:07:23 -0500

    second exercise

---
+++ src/exercises/ex2/escape_room_server.py
@@ -0,0 +1,28 @@
+# Escape room server
+import socket
+import random
+from escape_room import EscapeRoom
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50005              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
+    while True:
+        conn, addr = server_socket.accept()
+        # create an escape room
+        escape_room = EscapeRoom()
+        escape_room.start()
+        # while the escape room status is locked
+        # read the data from conn
+        with conn:
+            print('Connected by', addr)
+            while escape_room.status() == "locked":
+                data = conn.recv(1024)
+                output = escape_room.command(data.decode())  # encode converts from bytes to string
+                # send the output.encode() to conn (encode converts from string to bytes)
+                if escape_room.status() == "escaped" or escape_room.status() == "dead":
+                    conn.send(escape_room.status().encode())
+                else:
+                    conn.send(output.encode())
+            conn.close()
