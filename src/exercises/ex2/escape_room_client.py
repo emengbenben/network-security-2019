dd7a6f61472b45a0bb993531fb7e0efbba1245b0
Benfang Wang <bwang54@jhu.edu>
2019-02-05 21:59:16 -0500

    Update escape_room_client.py

--- src/exercises/ex2/escape_room_client.py
+++ src/exercises/ex2/escape_room_client.py
@@ -1,13 +1,13 @@
-# Echo client program
+# escape_room client program
 import socket
 
 HOST = '127.0.0.1'    # The remote host
-PORT = 50005              # The same port as used by the server
+PORT = 5000              # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((HOST, PORT))
 
     response = ""
-    while response != "escaped" and response != "dead":
+    while "escaped" not in response and "dead" not in response:
         command = input(">> ")
         client_socket.send(command.encode())
         data = client_socket.recv(1024)
