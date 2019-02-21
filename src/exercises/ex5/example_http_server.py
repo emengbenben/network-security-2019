52b86b7d9b6d916778e5fa0194d58dab9c5c8563
Benfang Wang <bwang54@jhu.edu>
2019-02-20 20:42:45 -0500

    Update example_http_server.py
    accomplish three extra credit: 
    More Mime Types - Use Python's MimeTypes module to return the correct type for different types of files based on extension
    index.html - If a URI is a directory, check if an index.html file is present. If so, return that instead of not found
    Last-Modified - Use the stat function to return the actual last modified time of the file in the last modified response header

--- src/exercises/ex5/example_http_server.py
+++ src/exercises/ex5/example_http_server.py
@@ -2,7 +2,17 @@ import asyncio
 import sys
 import datetime
 import os.path
+import time
+import mimetypes
 from pathlib import Path
+import glob
+import stat
+
+def getMdate(file):
+    '''return: modified time of a file'''
+    # os.stat return properties of a file
+    tmpTime = time.localtime(os.stat(file).st_mtime)
+    return time.strftime('%Y-%m-%d', tmpTime)
 
 class ExampleHttpServer(asyncio.Protocol):
 
@@ -24,8 +34,10 @@ class ExampleHttpServer(asyncio.Protocol):
         print('Connection from {}'.format(peername))
         self.transport = transport
 
+    def connection_lost(self, exc):
+        # The socket has been closed, stop the event loop
+        loop.stop()
     def data_received(self, data):
-        print(data.decode())
         self.buffer += data
         if not self.has_full_packet(self.buffer): #check if buffer have "\r\n\r\n"
             return
@@ -36,29 +48,60 @@ class ExampleHttpServer(asyncio.Protocol):
             first_list = first_string.split(" ")
 
             error_string = "HTTP/1.1 404 Not Found" +"Date: "+str(datetime.datetime.now())+"\r\n" + "Content-Length: 0\r\n" + "Server: NetSec Prototype Server 1.0\r\n\r\n"
+
+
+
             if first_list[0] == "GET":
                 if first_list[2] == "HTTP/1.1" or first_list[2] == "HTTP/1.0":
                     uri_string = self.document_root + first_list[1]
                     my_file = Path(uri_string)
 
                     if my_file.is_file():
+                        #get the size of the file
                         fsize = os.path.getsize(uri_string)
                         file = open(uri_string, mode='rb')
                         data = file.read()
-                        str1 = "HTTP/1.1 200 OK\r\n Date: "+str(datetime.datetime.now())+"\r\n Server: NetSec Prototype Server 1.0\r\n Last-Modified: "+str(datetime.datetime.now())+"\r\n"
-                        response = str1 + "Content-Length: "+ str(fsize) +"\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n"
+
+                        modify_time = getMdate(uri_string)
+
+                        # Use Python's MimeTypes module to return the correct type for different types of files based on extension
+                        name_list = first_list[1].split("/")
+                        file_name = mimetypes.guess_type(name_list[-1])
+
+                        str1 = "HTTP/1.1 200 OK\r\nDate: "+str(datetime.datetime.now())+"\r\nServer: NetSec Prototype Server 1.0\r\nLast-Modified: "+str(modify_time)+"\r\n"
+                        response = str1 + "Content-Length: "+ str(fsize) +"\r\nConnection: close\r\nContent-Type: "+ file_name[0]+"\r\n\r\n"
+                        print(response)
                         self.transport.write(response.encode())
                         self.transport.write(data)
+                    elif my_file.is_dir(): #if uri is a directory
+                        uri_string += "index.html"
+                        my_newfile = Path(uri_string)
+
+                        if my_newfile.is_file():  # if index.html is present
+                            fsize = os.path.getsize(uri_string)
+                            file = open(uri_string, mode='rb')
+                            data = file.read()
+                            # identify the type of the file- idnex.html
+                            file_name = mimetypes.guess_type("index.html")
+
+                            modify_time = getMdate(uri_string)
+
+                            str1 = "HTTP/1.1 200 OK\r\n Date: " + str(datetime.datetime.now()) + "\r\n Server: NetSec Prototype Server 1.0\r\n Last-Modified: " + str(modify_time) + "\r\n"
+                            response = str1 + "Content-Length: " + str(fsize) + "\r\nConnection: close\r\nContent-Type: " + file_name[0] + "\r\n\r\n"
+                            self.transport.write(response.encode())
+                            self.transport.write(data)
+                        else:
+                            self.transport.write(error_string.encode())
                     else:
                         self.transport.write(error_string.encode())
                 else:
                     self.transport.write(error_string.encode())
             else:
                 self.transport.write(error_string.encode())
+            self.buffer = bytes()
 
-
-# /Users/wang/PycharmProjects/netsec/www_server
-document_root =  sys.argv[1] # first command line parameter
+# example_http_server.py "/Users/wang/PycharmProjects/netsec/www_server"
+document_root = sys.argv[1] # first command line parameter
 loop = asyncio.get_event_loop()
 coro = loop.create_server(lambda: ExampleHttpServer(document_root), '127.0.0.1', 8280)
 server = loop.run_until_complete(coro)
